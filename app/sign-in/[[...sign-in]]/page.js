1f7954e9b645e59858e666306eab30ae0d3eeaf6
almustanir <olajidemubarak561@gmail.com>
2024-04-15 12:22:05 +0100

    auth

---
+++ app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,16 @@
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <>
+      <div className="">
+      <Image src="/uberbanner.webp" width={900} height={1000} className="object-contain h-full w-full"/>
+      <div className="absolute top-10 right-0">
+      <SignIn />
+      </div>
+        
+      </div>
+    </>
+  );
+}
