eb537e0ec06cef1d47cf049de141ede6740a3166
almustanir <olajidemubarak561@gmail.com>
2024-04-15 16:00:31 +0100

    auto

--- components/Home/InputItem.js
+++ components/Home/InputItem.js
@@ -1,37 +1,55 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
-function InputItem({type}) {
-    const [value, setValue] = useState(null);
+function InputItem({ type }) {
+  const [value, setValue] = useState(null);
+  const [placeholder, setPlaceholder] = useState(null);
+
+  useEffect(() => {
+    type == "source"
+      ? setPlaceholder("Pickup location")
+      : setPlaceholder("Dropoff location");
+  }, []);
+
+  const getLatAndLng = (place, type) => {
+    console.log(place,type)
+  };
   return (
     <div className="bg-slate-200 p-3 rounded-lg mt-3 flex items-center gap-4">
-      <Image src={type=='source'?"/source.png":'/dest.png'} width={15} height={15} />
+      <Image
+        src={type == "source" ? "/source.png" : "/dest.png"}
+        width={15}
+        height={15}
+      />
       {/* <input
         type="text"
         placeholder={type=='source-removebg'?"Pickup Location": "Dropoff Location"}
         className="bg-transparent w-full outline-none"
       /> */}
       <GooglePlacesAutocomplete
-      apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+        apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
         selectProps={{
           value,
-          onChange: setValue,
-          placeholder: 'Pickup Location',
-          isClearable:true,
-          className:'w-full',
-          components:{
-            DropdownIndicator:false
+          onChange: (place) => {
+            getLatAndLng(place, type);
+            setValue(place);
           },
-          styles:{
+          placeholder: placeholder,
+          isClearable: true,
+          className: "w-full",
+          components: {
+            DropdownIndicator: false,
+          },
+          styles: {
             control: (provided) => ({
-                ...provided,
-                backgroundColor: '#00ffff00',
-                border:'none'
+              ...provided,
+              backgroundColor: "#00ffff00",
+              border: "none",
             }),
-          }
+          },
         }}
       />
     </div>
