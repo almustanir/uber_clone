bb8391c4f060ef264969417ffb054b3c6a2eef6f
almustanir <olajidemubarak561@gmail.com>
2024-04-16 15:36:25 +0100

    location

--- components/Home/InputItem.js
+++ components/Home/InputItem.js
@@ -1,28 +1,48 @@
 "use client";
 
+import { DestinationContext } from "@/context/DestinationContext";
+import { SourceContext } from "@/context/SourceContext";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 function InputItem({ type }) {
   const [value, setValue] = useState(null);
   const [placeholder, setPlaceholder] = useState(null);
-
+  const { source, setSource } = useContext(SourceContext);
+  const { destination, setDestination } = useContext(DestinationContext);
   useEffect(() => {
     type == "source"
       ? setPlaceholder("Pickup location")
       : setPlaceholder("Dropoff location");
   }, []);
 
-  const getLatAndLng = (place, type) =>{
+  const getLatAndLng = (place, type) => {
     const placeId = place.value.place_id;
-    const service=new google.maps.places.PlacesService(document.createElement("div"));
-    service.getDetails({placeId},(place,status) =>{
-        if(status==='OK' && place.geometry && place.geometry.location)
-        {
-            console.log(place.geometry.location.lng())
+    const service = new google.maps.places.PlacesService(
+      document.createElement("div")
+    );
+    service.getDetails({ placeId }, (place, status) => {
+      if (status === "OK" && place.geometry && place.geometry.location) {
+        console.log(place.geometry.location.lng());
+        if (type == "source")
+         {
+          setSource({
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng(),
+            name:place.formatted_address,
+            label:place.name
+          })
+        } else {
+          setDestination({
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng(),
+            name:place.formatted_address,
+            label:place.name
+          })
         }
-    })
+      }
+    });
   };
   return (
     <div className="bg-slate-200 p-3 rounded-lg mt-3 flex items-center gap-4">
--- components/Home/SearchSection.js
+++ components/Home/SearchSection.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import InputItem from "./InputItem";
+import { SourceContext } from "@/context/SourceContext";
+import { DestinationContext } from "@/context/DestinationContext";
 
 function SearchSection() {
+  const { source, setSource } = useContext(SourceContext);
+  const { destination, setDestination } = useContext(DestinationContext);
+
+  
   return (
     <div className="p-2 md:pd-6 border-[2px] rounded-xl">
       <p className="text-[20px] font-bold">Get a ride</p>
